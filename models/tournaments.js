b63f9caf9ab651b9570640695568d3100800cd38
Felipe Chama <felipechama42@gmail.com>
2019-05-29 01:38:11 -0300

    Model Tournament: fix date when save tournament

--- models/tournaments.js
+++ models/tournaments.js
@@ -30,7 +30,6 @@ Tournament.add({
   },
   stepsDate: {
     type: Types.DateArray,
-    utc: true,
     label: 'Data das etapas',
   },
   score: {
@@ -38,6 +37,19 @@ Tournament.add({
     label: 'Ranking de pontos',
     note: 'Classifique de ordem crescente a classificação do ranking para o torneio. O último valor cadastrado será utilizado para as posições subsequentes.',
   },
+});
+
+// Fix date when save
+Tournament.schema.pre('save', function(next) {
+  this.stepsDate = this.stepsDate.map(date => {
+    const newDate = date;
+    newDate.setHours(12, 0, 0, 0);
+    newDate.setDate(newDate.getDate() + 1);
+
+    return newDate;
+  });
+
+  next();
 });
 
 Tournament.defaultColumns = 'name, startAt';
